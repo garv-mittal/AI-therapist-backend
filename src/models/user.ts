21190345bc38ec24e34a88dc638798d7a0e5e1e9
garv-mittal <mittal04garv@gmail.com>
2025-09-04 01:25:51 +0530

    feat: add user and session models with Mongoose schemas

---
+++ src/models/user.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface UserT extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema = new Schema<UserT>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model<UserT>("User", UserSchema);
